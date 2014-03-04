5b4d04c9d8882a0269211b016376a2f5c043419a
Linh Vo <hailinh.vo@gmail.com>
2014-03-03 21:26:47 -0600

    [#66795346] get link github @tvu review

--- myproject/views.py
+++ myproject/views.py
@@ -1,7 +1,7 @@
 import json
 import re
 from django.http import HttpResponse
-from django.shortcuts import render_to_response, render
+from django.shortcuts import render
 from django.conf import settings
 from django.template import loader, RequestContext
 from django.views.decorators.http import require_http_methods, require_POST
@@ -23,9 +23,12 @@ def pt_hook(request):
     pv_link = data['primary_resources'][0]['url']
     project_name = data['primary_resources'][0]['name']
     story_type = data['primary_resources'][0]['story_type']
-    # estimate = data['']
     message = data['message']
     github_link = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message)
+    if github_link:
+        github_link = github_link[0]
+    else:
+        github_link = None
     if found_story:
         found_story = Review.objects.filter(story_id=story_id)[0]
         if status == 'delivered':
@@ -33,14 +36,15 @@ def pt_hook(request):
             return HttpResponse("Story was delivered")
         else:
             for user in users:
-                if 'review' and user.name in message:
-                    review_by = user.id
+                if 'review' and user.tag_name in message:
+                    review_by = user
                     found_story.review_by = review_by
                     found_story.created_timestamp=created_timestamp
                     found_story.created_by = created_by
                     found_story.pv_link = pv_link
                     found_story.project_name = project_name
                     found_story.status = status
+                    found_story.github_link = github_link
                     found_story.save()
                     return HttpResponse('Update story')
     else:
@@ -49,12 +53,11 @@ def pt_hook(request):
                 review_by = user
                 Review.objects.create(created_timestamp=created_timestamp, created_by=created_by, pv_link=pv_link,
                                       project_name=project_name, status=status, story_type=story_type, story_id=story_id,
-                                      review_by=review_by)
+                                      review_by=review_by, github_link=github_link)
     return HttpResponse("Got Data")
 
 
 def home(request):
-    print 'test'
     users = User.objects.all()
     results = []
     for user in users:
